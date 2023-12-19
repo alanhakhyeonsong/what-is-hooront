7b13a066d4f2eb42e70f0bfbcda11efafa0d5aaf
alanhakhyeonsong <songs4805@naver.com>
2023-12-19 16:12:49 +0900

    Feat: main page에 대한 React Router 추가

---
+++ src/router/index.ts
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { allRoutes } from './routes';
+
+export const router = createBrowserRouter(allRoutes);
---
+++ src/router/routes/index.tsx
@@ -0,0 +1,13 @@
+import { RouteObject } from 'react-router-dom';
+
+import App from '@/App';
+
+export const allRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      
+    ]
+  }
+]
