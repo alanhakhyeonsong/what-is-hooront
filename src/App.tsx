1ec3178ac3c74080235b8c99e0c2f57cdcfb837f
alanhakhyeonsong <songs4805@naver.com>
2023-12-19 22:25:47 +0900

    Refactor: App.tsx 내에 Outlet 외 모두 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { Link,Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <h1>Main Page</h1>
-      <div>Ramos의 React + TypeScript 삽질</div>
-      <div>
-        <Link to='/example1'>example1 페이지</Link>
-      </div>
       <Outlet />
-      <div>
-        <Link to='/example2'>example2 페이지</Link>
-      </div>
     </>
   )
 }
