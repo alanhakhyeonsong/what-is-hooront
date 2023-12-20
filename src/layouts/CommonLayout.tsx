09a43b7543932976e6e229313c53279cb5fde174
alanhakhyeonsong <songs4805@naver.com>
2023-12-20 11:18:25 +0900

    Refactor: line 정리

--- src/layouts/CommonLayout.tsx
+++ src/layouts/CommonLayout.tsx
@@ -1,7 +1,7 @@
 import { Layout, Menu } from 'antd';
 import { Content, Footer, Header } from 'antd/es/layout/layout';
 import { Suspense } from 'react';
-import { Link,Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const items = [
   {
